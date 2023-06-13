faf65a13c0ce6194bf21924fb0f285d674b1e955
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-06-13 18:21:15 +0100

    feat: add list user Desativados

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CriarUsuarioController } from "../modules/usuario/CriarUsuario/CriarUsuarioController";
 import { ListarUsuarioController } from "../modules/usuario/ListarUsuario/ListarUsuarioController";
 import { AtualizarUsuarioController } from "../modules/usuario/AtualizarUsuario/AtualizarUsuarioController";
+import { ListarUsuarioDesativadoController } from "../modules/usuario/ListarUsuarioDesativado/ListarUsuarioDesativadoController";
 
 
 const usuarioRouter = Router()
@@ -9,9 +10,11 @@ const usuarioRouter = Router()
 const criarUsuario = new CriarUsuarioController()
 const atualizarUsuario = new AtualizarUsuarioController()
 const ListarUsuario = new ListarUsuarioController()
+const ListarUsuarioDesativado = new ListarUsuarioDesativadoController()
 
 usuarioRouter.post("/", criarUsuario.handle)
 usuarioRouter.get("/", ListarUsuario.handle)
+usuarioRouter.get("/Desativdos", ListarUsuarioDesativado.handle)
 usuarioRouter.put("/:id", atualizarUsuario.handle)
 
-export {usuarioRouter }
+export { usuarioRouter }
